1b37838810b5aa8400baa5044645591f2e70a153
Sunty Eljohn <163456176+kr4fer@users.noreply.github.com>
2025-05-31 21:27:34 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "It\tNational High School Grading system\n";
+    // Prompt student Info
+    cout << "Enter student Name:
+    getline(cin, studentName);
+    cout << "Enter Subject: getline (cin, subject);
+    // Get Quiz Info
+    cout << "It\t\t Quiz\n";
+    cout << "Enter Raw Score: ";
+    cin >> quiz.rawscore;
+    cout << "Total Items: "; cin >> quiz.totalItems;
+    cout << "Enter Quiz Percent: " cin >> quiz.percentage;
+    // Get Assignment Info
+    cout << "It\t\t Assignment \n";
+    cout << "Enter Assignment Equivalent: cin >> assignment .equivalent;
+    cout << "Enter Assignment Percent: cin >> assignment .percentage;
+    / Get Exam Info cout <‹ "\t\t\t Exam\n"; cout <‹ "Enter Raw Score: cin >> exam.rawscore;
+    cout <‹ "Total Items: "; cin >> exam.totalitems;
+    cout << "Enter Exam Percent: cin >> exam. percentage:
+
+    return 0;
+}
